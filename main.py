06ff283516ad8634987f09e4d4c9fc5b57506fe7
SeraKah-1 <farreladitya38@gmail.com>
2026-02-09 17:39:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import streamlit as st
 from ui_modern import render_app_modern
 
-st.set_page_config(page_title="Quiz Gen", page_icon="✏️", layout="centered")
+# Config Halaman (Harus di paling atas)
+st.set_page_config(
+    page_title="Quiz App", 
+    page_icon="⚡", 
+    layout="centered",
+    initial_sidebar_state="collapsed"
+)
 
 if __name__ == "__main__":
     render_app_modern()
